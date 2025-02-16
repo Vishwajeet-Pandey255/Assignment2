c5e467a502e3c84ba45978ea1f19e41278466a2d
Manish yadav <manish781188@gmail.com>
2025-02-16 22:52:49 +0530

    prime

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function check(num) {
-
+ let count = 0;
+ for (let i = 2; i <num;i++){
+  if (i%2==0)coutn++;
+ }
+ return count;
 
 }
 
-check(5);
+let count=check(5);
+
+if(count==0){
+    console.log("prime");
+    
+}else{
+    console.log("not a prime");
+    
+}
